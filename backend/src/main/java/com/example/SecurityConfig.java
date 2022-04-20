a1bf60b03ebbf6e7d128e45c47650b7e3421012c
정현수 <wjdgustn15@kakao.com>
2022-04-20 14:44:06 +0900

    b/build : add SecurityConfig & disable login form

---
+++ backend/src/main/java/com/example/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example;
+
+//TODO 상황에 맞게 폴더 위치를 수정, 추가 변경해주세요.
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().exceptionHandling();
+        http.formLogin().disable();
+    }
+}
