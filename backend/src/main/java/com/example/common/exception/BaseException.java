a0f0228010a69c90253a262bdaa6d95b2296652a
정현수 <wjdgustn15@kakao.com>
2022-04-25 13:55:35 +0900

    b/feat : change Exception -> RuntimeException

--- backend/src/main/java/com/example/common/exception/BaseException.java
+++ backend/src/main/java/com/example/common/exception/BaseException.java
@@ -1,10 +1,13 @@
 package com.example.common.exception;
 
 import com.example.common.dto.ErrorResponse;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-public class BaseException extends Exception {
+@NoArgsConstructor(access=AccessLevel.PROTECTED)
+public class BaseException extends RuntimeException {
 
     public BaseException(String message) {
         super(message);
