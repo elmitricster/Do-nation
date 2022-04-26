b360504a0191040599a68e83b0483fad90ee30b8
정현수 <wjdgustn15@kakao.com>
2022-04-26 10:55:15 +0900

    b/feat : can test in h2 keyword

--- backend/src/test/java/com/example/payment/repository/PaymentRecordRepositoryTest.java
+++ backend/src/test/java/com/example/payment/repository/PaymentRecordRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDateTime;
@@ -15,6 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class PaymentRecordRepositoryTest {
     @Autowired
     PaymentRecordRepository paymentRecordRepository;
