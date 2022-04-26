e5f5b14f70000dea039ead304fea873ce87edccc
김준석 <jor9511@naver.com>
2022-04-26 17:53:23 +0900

    f/feat: profile header 디자인

--- frontend/src/containers/profile/ProfileContainer.js
+++ frontend/src/containers/profile/ProfileContainer.js
@@ -1,19 +1,19 @@
-import { ProfileHeader } from "components/profile/Profile/ProfileHeader";
-import { Outlet, useNavigate, useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { ProfileHeader } from 'components/profile/ProfileHeader';
+import { Outlet, useNavigate, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export function ProfileConatiner() {
   const navigate = useNavigate();
   const params = useParams();
 
   useEffect(() => {
-    navigate(`/profile/${params.nickname}/articles`)
-  })
+    navigate(`/profile/${params.nickname}/articles`);
+  }, []);
 
-  return(
-    <div>
+  return (
+    <div className="container">
       <ProfileHeader />
       <Outlet />
     </div>
-  )
+  );
 }
