4182618050c0680b01c2689f5093d29741f18b31
강민철 <kmc9308@naver.com>
2022-05-06 11:04:54 +0900

    f/feat : kakao login jwt token save

--- frontend/src/containers/user/AuthContainer.jsx
+++ frontend/src/containers/user/AuthContainer.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export function AuthContainer() {
+  const navigate = useNavigate();
   const code = new URL(window.location.href).searchParams.get('code');
-  return <div>{code}</div>;
+  useEffect(() => {
+    axios
+      .get(`http://k6c101.p.ssafy.io:8080/auth/login?code=${code}`)
+      .then(res => {
+        localStorage.setItem('jwt', res.data.token);
+        navigate('/');
+      })
+      .catch(err => {
+        console.log(err);
+        navigate('/user/login');
+      });
+  });
+
+  return <div></div>;
 }
