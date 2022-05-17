1de758c12425c6ecb8eef00c57cdb3b810085e9a
정현수 <wjdgustn15@kakao.com>
2022-05-17 15:22:04 +0900

    ci/feat : add docker swarm setting

--- nginx&deploy/deploy.sh
+++ nginx&deploy/deploy.sh
@@ -1,24 +1,14 @@
 #!/bin/sh
 
-
-# 정지
-sudo docker-compose stop fe
-sudo docker-compose stop be
-sudo docker-compose stop nginx
-
-# 삭제
-sudo docker-compose rm fe --force
-sudo docker-compose rm be --force
-sudo docker-compose rm nginx --force
-
-
-
 sudo docker pull wjdgustn15/ssafy_donation_service:fe
 sudo docker pull wjdgustn15/ssafy_donation_service:be
 sudo docker pull wjdgustn15/ssafy_donation_service:nginx
 
 #갱신
-sudo docker-compose up -d
+sudo docker stack deploy -c docker-compose.yml donation-service-stack
+sudo docker service scale donation-service-stack_be=3
+sudo docker service scale donation-service-stack_fe=3
+
 
 # 불필요한 이미지 죽이기.
 sudo docker system prune --force
