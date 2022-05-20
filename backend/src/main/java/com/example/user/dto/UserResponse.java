4b4fc4eb50cc6a7edf4fde9a2450339cfdab0977
정현수 <wjdgustn15@kakao.com>
2022-05-20 09:58:29 +0900

    b/fix : userResponse user -> user's fields

--- backend/src/main/java/com/example/user/dto/UserResponse.java
+++ backend/src/main/java/com/example/user/dto/UserResponse.java
@@ -10,10 +10,36 @@ import java.util.stream.Collectors;
 
 @Getter
 public class UserResponse {
-    private User user;
+    private Long id;
+    private String platformType;
+    private String kakaoId;
+    private String profileImage;
+    private String profileName;
+    private String introMessage;
+    private String category;
+    private String birthday;
+    private String nickname;
+    private boolean isCertified;
+    private int point;
+    private String subject;
+
     private List<UserUrlDto> userUrls;
     public UserResponse(User user, List<UserUrl> url) {
-        this.user = user;
+        makeUser(user);
         this.userUrls =  url.stream().map(x->new UserUrlDto(x.getUserUrl(),x.getUrlName())).collect(Collectors.toList());
+    }
+    private void makeUser(User user){
+        this.id = user.getId();
+        this.platformType = user.getPlatformType();
+        this.kakaoId = user.getKakaoId();
+        this.profileImage = user.getProfileImage();
+        this.profileName = user.getProfileName();
+        this.introMessage = user.getIntroMessage();
+        this.category = user.getCategory();
+        this.birthday = user.getBirthday();
+        this.nickname = user.getNickname();
+        this.isCertified = user.isCertified();
+        this.point = user.getPoint();
+        this.subject = user.getSubject();
     }
 }
